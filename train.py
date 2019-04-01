1a44b22092b4a6b0a8ac1bb63c4ca96d4377e24c
mickey <mickey.a.warner@gmail.com>
2019-04-01 11:57:54 -0600

    obtain training data

--- train.py
+++ train.py
@@ -1,31 +1,20 @@
 import os
-import time
 import mysql.connector
-import requests
 import datetime
+import numpy as np
+import pandas as pd
 
 ### Get credentials
 with open(os.path.expanduser("~/.secrets/creds")) as f:
     username, password, _ = f.read().split("\n")[:3]
 
-###
+### Connect to database
 mydb = mysql.connector.connect(
     host = "localhost",
     user = username,
     passwd = password,
     database = "finance")
 
-# A better way for getting Decimal class
-class myconverter(mysql.connector.conversion.MySQLConverter):
-    def _NEWDECIMAL_to_python(self, value, desc=None):
-        return float(value)
-
-mydb.set_converter_class(myconverter)
-mycursor = mydb.cursor()
-
-mycursor.execute("select * from stocks limit 10;")
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
-
+### Get training data
+x = pd.read_sql("select * from stocks;", con = mydb)
 
