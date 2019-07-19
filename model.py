113a4b11c3b7c7819ad384d72b2d3c7faf75676c
mickey <mickey.a.warner@gmail.com>
2019-07-18 19:09:54 -0600

    data processing

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import numpy as np
 import requests
+from datetime import date
 
 ### Parse arguments
 APIKEY = sys.argv[1]
@@ -24,9 +26,68 @@ call = endpoint + "?" + params.format("TIME_SERIES_DAILY_ADJUSTED",
 
 ### Do the API call to AlphaVantage
 r = requests.get(call)
+
+# Should probably having some error checking here
+
+# Feature names
+# 0 date
+# 1 open
+# 2 high
+# 3 low
+# 4 close
+# 5 adjusted close
+# 6 volume
+# 7 dividend amount
+# 8 split coefficient
+
+# Format response into numpy object
 rlist = r.text.rstrip().split("\r\n")[1:]
 m = len(rlist)
-for line in reversed(rlist):
-    print(str(tuple([SYMBOL] + line.split(","))))
+k = 9 # Known number of columns
+
+x = np.zeros((m, k))
+for i in range(m):
+    line = rlist[i].split(",")
+    d = line[0]
+    # Convert date to number of days since 1 Jan 2000
+    x[i, 0] = (date(int(d[0:4]), int(d[5:7]), int(d[8:10])) - date(2000, 1, 1)).days
+    # Convert remaining values to floats
+    x[i, 1:] = line[1:]
+
+# Reverse order so the earlier days are at the top
+x = np.flip(x, 0)
+
+# Get response (adjusted close)
+y = x[:,5]
+
+# Get test data (should be today's date)
+testx = x[-1, :]
+
+# Offset by one day
+# The prediction is tomorrow's close
+y = y[1:]
+x = x[:-1, :]
+
+
+### Scale the features to [0, 1], remove any singularites
+xmin = np.min(x, 0)
+xmax = np.max(x, 0)
+ymin = np.min(y, 0)
+ymax = np.max(y, 0)
+
+tmpind = xmax - xmin != 0
+x = x[:, tmpind]
+testx = testx[tmpind]
+xmin = xmin[tmpind]
+xmax = xmax[tmpind]
+
+x = (x - xmin) / (xmax - xmin)
+testx = (testx - xmin) / (xmax - xmin)
+y = (y - ymin) / (ymax - ymin)
+
+
+### Train a model
+
+
 
 sys.stdout.flush()
